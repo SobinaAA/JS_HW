359c7e96948ad9bc3760b2dd92fa1f8754f8c2ff
Alena <makkuro@gmail.com>
2024-09-13 18:16:56 +0500

    added 2 tasks

---
+++ 2 task.js
@@ -0,0 +1,14 @@
+//3 task*
+//Работает в консоли браузера, так как запрашивается ввод числа от пользователя
+"use strict";
+let result_task = -1, n_ask;
+while (result_task < 0) {
+    console.log('Начинаю работу, бррбрбр..')
+    n_ask = +prompt('Введите число N для вывода по заданию! \n N - целое число от 0 до 9  \n N + NN + NNN');
+    if (n_ask != NaN && n_ask < 10 && n_ask > 0 && Number.isInteger(n_ask)) {
+        result_task = n_ask + Number("" + n_ask + n_ask) + Number("" + n_ask + n_ask + n_ask);
+        console.log('Получите ваше число: ' + result_task);
+    } else {console.log('Что-то пошло не так...')}
+}
+
+
