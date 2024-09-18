ec6169660fdc494c94a00fd00ae786ece71946b4
Alena <makkuro@gmail.com>
2024-09-18 14:09:01 +0500

    added folder

---
+++ 2HW/3_task.js
@@ -0,0 +1,18 @@
+"use strict";
+const minAge = 18;
+const maxAge = 60;
+let age_ask = prompt('Введите свой возраст! \n Надоест, введите "end"');
+while (age_ask != 'end') {
+    if (isNaN(age_ask)) {
+        alert('Incorrect data type');
+    } else if (age_ask < minAge) {
+        alert("You don't have access cause your age is " + age_ask + " It's less then " + minAge);
+    } else if (age_ask >= minAge && age_ask < maxAge) {
+        alert("Welcome!");
+    } else if (age_ask >= maxAge) {
+        alert("Keep calm and look Culture channel");
+    } else {
+        alert("Technical work");
+    }
+    age_ask = prompt('Введите свой возраст! \n Надоест, введите "end"');
+}
