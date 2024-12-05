342ab1893e2b2a3ee584c18df02e77597624aa79
Alena <makkuro@gmail.com>
2024-12-05 09:30:51 +0500

    Added HW

---
+++ 18HW/1_2_task/src/data/labels.ts
@@ -0,0 +1,3 @@
+export const headerH4 = 'Dynamic Controls';
+export const message1 = `It's gone!`;
+export const message2 = `It's back!`;
---
+++ 18HW/1_2_task/src/data/selectors.ts
@@ -0,0 +1,5 @@
+export const linkSelector = (linkName: string) => `a[href="/${linkName}"]`;
+export const removeButtonSelector = 'button[onclick="swapCheckbox()"]';
+export const headerSelector = '//h4[1]';
+export const checkboxSelector = "input[type='checkbox']";
+export const messageSelector = 'p#message';
